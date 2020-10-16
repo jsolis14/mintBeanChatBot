a4864aba67ce08069f2e3f74cdae8a217e1ca298
Jesse Solis <jesse.solis0316@gmail.com>
2020-10-16 15:15:48 -0500

    created sample data

---
+++ features/data.js
@@ -0,0 +1,96 @@
+const data = {
+    'technologies': {
+        JavaScript: ['Health Nut', 'Flippy Flapp', 'Dotify', 'Aircnc'],
+        Python: ['Health Nut', 'Flippy Flapp'],
+        Express: ['Aircnc'],
+        Flask: ['Health Nut', 'Flippy Flapp'],
+        React: ['Health Nut', 'Flippy Flapp', 'Dotify'],
+        Redux: ['Health Nut', 'Flippy Flapp'],
+        PostgreSQL: ['Health Nut', 'Flippy Flapp', 'Aircnc']
+    },
+    'Personal Info': {
+        State: 'Texas',
+        City: 'Fort Worth',
+        Age: '23',
+        gender: 'Male',
+    },
+    'Education': {
+        'Texas A&M University': {
+            location: 'College Station, Texas',
+            started: 'May 2015',
+            graduated: 'Dec 2019',
+            degree: 'Bachelor of Science',
+            major: 'Industrial Engineering'
+        },
+        'App Academy': {
+            location: 'San Francisco, CA',
+            started: 'Jan 2020',
+            graduated: 'Jun 2020',
+            degree: '',
+            major: 'Web Development'
+        }
+    },
+    'Projects': {
+        'Health Nut': {
+            description: 'Health Nut is an app where users can choose from multiple fitness plans, such as loosing, maintaining, or gaining weight and then track the calories needed to accomplish the fitness plan on a daily basis',
+            tehnologies: [
+                'React',
+                'Redux',
+                'Auth0',
+                'Material UI',
+                'Heroku Deployment (backend)',
+                'AWS Deployment (front-end)',
+                'Python',
+                'JavaScript',
+                'Flask',
+                'PostgreSQL',
+                'SQLAlchemy'],
+            'live link': 'https://master.d2v8iaichtof5r.amplifyapp.com/login',
+            'repo link': 'https://github.com/jsolis14/HealthNut',
+
+        },
+        'Flippy Flapp': {
+            description: 'Flippy Flapp is a website where private pilots can save and save thier flight plans. Users can choose between two airports on the map and a corresponding set of waypoints will be generated for refueling purposes.',
+            tehnologies: [
+                'React',
+                'Redux',
+                'Auth0',
+                'Material UI',
+                'Heroku Deployment (backend)',
+                'AWS Deployment (front-end)',
+                'Python',
+                'JavaScript',
+                'Flask',
+                'PostgreSQL',
+                'SQLAlchemy'
+            ],
+            'live link': 'https://www.flippyflapp.com/',
+            'repo link': 'https://github.com/jballa91/flippyFlapp',
+        },
+        'Dotify': {
+            description: 'Dotify is a front end client that uses the Spotify API to preform certain actions on behalf of a spotify user. Currently a user can access thier playlist, create a playlist, play music, and even search for songs and artist. The user can even create a spotify connect device that can be controlled across other spotify platforms.',
+            tehnologies: [
+                'React',
+                'OAuth',
+                'React Bootstrap',
+                'Heroku Deployment',
+                'Spotify Api',
+            ],
+            'live link': 'https://master.d36wh2xec4sh9g.amplifyapp.com/login',
+            'repo link': 'https://github.com/jsolis14/Dotify',
+        },
+        'Aircnc': {
+            description: 'AirCnC is a spin off of Airbnb where users can rent kitchens and hosts can rent out their kitchens to users.',
+            tehnologies: ['Heroku Deployment (backend/front-end)',
+                'JavaScript',
+                'Express',
+                'Google Maps API',
+                'PostgreSQL',
+                'Sequelize',
+                'Material Lite'
+            ],
+            'live link': 'http://www.aircnc.us/',
+            'repo link': 'https://github.com/JoshuaCachola/Rework-front',
+        }
+    }
+}
