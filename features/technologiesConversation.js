09204e62c63076e037821b3412d5ad2df72e40dd
Jesse Solis <jesse.solis0316@gmail.com>
2020-10-21 12:00:16 -0500

    added responsed to technology convo

--- features/technologiesConversation.js
+++ features/technologiesConversation.js
@@ -1,13 +1,23 @@
 const { Botkit, BotkitConversation } = require('botkit');
 const data = require('../data');
 
+function technologyReply(key) {
+    keys = ['javascript', 'python', 'react', 'redux', 'postgres', 'flask', 'express', 'html', 'css']
+
+    if (keys.findIndex(key) !== -1) {
+        return `The projects I have made using ${key} are ${data.technologies[keys[key]].split(',')}`
+    } else if (key === 'all') {
+        return `These are all the technologies which I have used: ${Object.keys(data).join(',')}`
+    } else {
+        return ''
+    }
+}
 
 module.exports = function (controller) {
     const MY_TECHNOLOGIES = 'technologies';
     let convo = new BotkitConversation(MY_TECHNOLOGIES, controller);
 
     convo.addAction('technology question')
-
     convo.addQuestion({
         text: 'what would you like to know about the technologies I know?',
         quick_replies: [{
@@ -49,7 +59,7 @@ module.exports = function (controller) {
         }],
     }, async function (response, convo, bot) {
         const key = response
-        await bot.say(personalInfoReply(key))
+        await bot.say(technologyReply(key))
 
     }, 'response', 'technology question');
 
